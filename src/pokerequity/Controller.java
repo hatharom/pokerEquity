89aa99678227c650829f6cec72ad9248b0f329da
Zoli <zahzol81@gmail.com>
2016-10-27 15:58:42 +0200

    gui2

--- src/pokerequity/Controller.java
+++ src/pokerequity/Controller.java
@@ -1,9 +1,19 @@
-
 package pokerequity;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import pokereval.*;
 
 public class Controller {
+
+    @FXML
+    Label errorLabel;
+
     static {
         System.out.println("dfdfdfdfd");
+    }
+
+    public void testing() {
+        
     }
 }
